7728a86d374ff5413415fdb9999b897d90ce44bb
curdog <curdog@gmail.com>
2013-02-20 14:42:31 -0500

    Added AI header,  designed ish main card palying process

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "deck.hpp"
+#include <iostream>
+
+
+int main() {
+  while( true ){
+    
+    //deal,
+    //bid
+    //game select card check suit
+    //pick winer
+    //assign points
+    
+  }
+  return 0;
+}
+
