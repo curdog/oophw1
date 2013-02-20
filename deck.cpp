f537135de556b5a54333ce483c6f84c0bcd5a799
curdog <curdog@gmail.com>
2013-02-20 13:13:28 -0500

    Fleshed out, Deck and Card Classes, gona start on the game..

--- deck.cpp
+++ deck.cpp
@@ -1,31 +1,44 @@
 #include "card.hpp"
 #include "deck.hpp"
 #include <cstdlib>
+#include <time.h>
+
 
 Deck::Deck(){
-  deck = new Deck[30];
+  deck = new Deck[DECK_SIZE];
   char base = '<'
-  for( int i = 0; i < 3; i++ ){
-    for( int j = 0; j < 10; j++ ){
+
+  for( int i = 0; i < SUITS; i++ ){
+    for( int j = 0; j < (DECK_SIZE / SUITS); j++ ){
       deck[i + j] = new Card( j, base + (char)i );
     }
   }
+
 }
 
 ~Deck::Deck(){
-
-delete
+  delete[] deck;
 }
 
 void Deck::shuffle(){
+  srand( time( 0 ) );
+ 
   for( int i = 0; i < 2000; i++ ){
-    
+    swap( deck[rand() % DECK_SIZE ], deck[rand() % DECK_SIZE ] );
   }
+  index = 0;
 }
 
-Card* Deck::getCard(){
-  if( index > 29 ){
+Card* Deck::deal(){
+  if( index > DECK_SIZE ){
     return 0;
-  }
-  return deck[++index];
+  }  
+  return deck[index++];
+}
+
+void Deck::swap( Card* a, Card* b){
+  Card* c;
+  c = a;
+  a = b;
+  b = c;
 }
--- deck.hpp
+++ deck.hpp
@@ -8,10 +8,12 @@ public:
   Deck();
   ~Deck();
   void shuffle();
-  Card* getCard();
+  Card* deal( int cards = 1);
 private:
+  void swap( Card*, Card* ); //internal helper function
   Card* deck;
-  int index;
-  const int size = 30;
+  int index; // for dealing purposes
+  const int DECK_SIZE = 30; //tempting to put as a define
+  const int SUITS = 3;
 };
 #endif
